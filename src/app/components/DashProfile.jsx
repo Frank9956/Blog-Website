c390ddc9fecad40e88dda789bc9913807863cd7b
Tilak Singh <113658838+Frank9956@users.noreply.github.com>
2025-04-25 13:35:20 +0530

    fixing

--- src/app/components/DashProfile.jsx
+++ src/app/components/DashProfile.jsx
@@ -1,19 +1,7 @@
-'use client';
-
-import { UserProfile } from '@clerk/nextjs';
-import { dark, light } from '@clerk/themes';
-import { useTheme } from 'next-themes';
+import React from 'react'
 
 export default function DashProfile() {
-  const { theme } = useTheme();
   return (
-    <div className='flex justify-center items-center w-full'>
-      <UserProfile
-        appearance={{
-          baseTheme: theme === 'dark' ? dark : light,
-        }}
-        routing='hash'
-      />
-    </div>
-  );
+    <div>DashProfile</div>
+  )
 }
