58c23a490dc8a830b62aa28427530084daacd682
Tilak Singh <113658838+Frank9956@users.noreply.github.com>
2025-04-29 16:35:39 +0530

    header ui

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -84,14 +84,15 @@ export default function Header() {
           {/* Search */}
           <form onSubmit={handleSubmit} className="hidden lg:flex items-center space-x-3">
             <Input
-              className="bg-background text-grey border placeholder:text-gray-500 text-lg px-4 py-2"
+              className="bg-background text-grey  placeholder:text-gray-500 text-lg px-4 py-2"
+              variant="ghost"
               type="text"
               placeholder="Search..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <Button type="submit" variant="outline" size="icon">
-              <Search className="w-5 h-5" />
+            <Button type="submit" variant="ghost" size="icon">
+              <Search className="w-7 h-7" />
             </Button>
           </form>
 
@@ -127,11 +128,10 @@ export default function Header() {
 
       {/* Mobile Menu */}
       <div
-        className={`overflow-hidden transition-all duration-300 md:hidden px-6 ${
-          isOpen ? 'max-h-[500px] opacity-100 mt-2' : 'max-h-0 opacity-0'
-        }`}
+        className={`overflow-hidden transition-all duration-300 md:hidden px-6 ${isOpen ? 'max-h-[500px] opacity-100 mt-2' : 'max-h-0 opacity-0 '
+          }`}
       >
-        <div className="flex flex-col gap-2 bg-white dark:bg-black rounded-md py-4 px-4">
+        <div className="flex flex-col gap-2 bg-transparent backdrop-blur-[10px]  rounded-md py-4 px-4 font-bold">
           {isLoading ? (
             <div>Loading categories...</div>
           ) : (
@@ -140,7 +140,8 @@ export default function Header() {
                 key={category.slug}
                 href={`/${category.slug}`}
                 onClick={handleLinkClick}
-                className={`${path === `/${category.slug}` ? 'font-bold' : ''}`}
+                className={`hover:text-orange-500 transition-colors ${path === `/${category.slug}` ? 'text-orange-600' : ''
+                  }`}
               >
                 {category.name}
               </Link>
@@ -149,11 +150,13 @@ export default function Header() {
           <Link
             href="/about"
             onClick={handleLinkClick}
-            className={`${path === '/about' ? 'font-bold' : ''}`}
+            className={`hover:text-orange-500 transition-colors ${path === '/about' ? 'text-orange-600' : ''
+              }`}
           >
             About Us
           </Link>
         </div>
+
       </div>
     </header>
   );
