93dc9316a179f1e9208d6b253f61c80a070d09eb
Tilak Singh <113658838+Frank9956@users.noreply.github.com>
2025-04-29 12:02:03 +0530

    fix error

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import { ThemeProvider } from 'next-themes';
 import { ClerkProvider } from '@clerk/nextjs';
 import Footer from './components/Footer';
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/sidebar';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
